957ff98285cea5ae2897c0c4e244c18db3271d42
ellisoalgo <ellisjwhitney@gmail.com>
2024-04-03 21:40:38 +0200

    ejercicios 2.1

---
+++ 2.1-intro-programacion/main.js
@@ -0,0 +1,30 @@
+// //Aquñi va el código JavaScript
+// 'use strict';
+
+// document.querySelector('h1').innerHTML = 'Hi';
+
+// //Ejercicio constantes
+// /*let myAddress = "Barcelona 08860";
+// myAddress = "Madrid, 28009";*/
+
+// //Ejercicio innerHTML
+// const titleElement = document.querySelector(".paragraph");
+// titleElement.innerHTML = titleElement.innerHTML + " mundo!";
+
+// //Ejercio seleccionar
+// const listElement = document.querySelector(".list");
+// const content = '<li>Ellis</li><li>Paula</li>';
+// listElement.innerHTML = content;
+
+// const listTitle = document.querySelector(".listTitle");
+// listTitle.innerHTML = listTitle.innerHTML + "Paula";
+
+
+// //Ejercicio contenido
+//const titlePage = document.querySelector(".div");
+//const content = '<h2>Lorem ipsum</h2> <img src="http://via.placeholder.com/350x150" alt="boceto de imagen"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+//titlePage.innerHTML = content;
+
+//Ejercicio classList
+const buttonOne = document.querySelector(".buttonOne");
+buttonOne.classList.add("off");
