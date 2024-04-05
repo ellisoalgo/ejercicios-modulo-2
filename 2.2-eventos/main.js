8dc7da461ce864c38eed2cb41c58ae19b4fd471b
ellisoalgo <ellisjwhitney@gmail.com>
2024-04-05 08:31:47 +0200

    modifications text input exercise

--- 2.2-eventos/main.js
+++ 2.2-eventos/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-/*
 //Ejercicio 2.2.2 - 1:
 // elemento de HTML
 const name = document.querySelector(".name");
@@ -18,16 +17,16 @@ const button2 = document.querySelector(".button2");
 button2.addEventListener("click", () => {
   button2.classList.toggle("buttonClick");
 });
-*/
 
-/*//Ejercicio 2.2.2 - 3
+
+//Ejercicio 2.2.2 - 3
 const paragraph = document.querySelector(".lorem");
 
 paragraph.addEventListener("click", () => {
   paragraph.innerHTML += `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam voluptatem, eos sapiente ipsum reprehenderit, tempore id quos odit culpa sequi, aliquam magnam natus praesentium adipisci temporibus ea? Quae, possimus error?`;
 
 });
-*/
+
 
 //Ejercicio 2.2.3 - 1
 const textInput = document.querySelector('.text');
